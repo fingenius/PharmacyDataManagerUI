47cbb8757ca90ff0e396fb50a9bbe9c7e606c076
Shantanu Pandey <shantanupandey1609@gmail.com>
2025-07-02 16:09:17 +0530

    create ejs files to handle paths

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -2,4 +2,30 @@ exports.getIndex = (req, res) => {
   res.render("index");
 };
 
+exports.getDoctors = (req, res) => {
+  res.render("categories/doctors");
+};
+
+exports.getPatients = (req, res) => {
+  res.render("categories/patients");
+};
+exports.getPharmaceuticalCompanies = (req, res) => {
+  res.render("categories/pharmaceutical-companies");
+};
+exports.getDrugs = (req, res) => {
+  res.render("categories/drugs");
+};
+exports.getPharmacies = (req, res) => {
+  res.render("categories/pharmacies");
+};
+exports.getPrescriptions = (req, res) => {
+  res.render("categories/prescriptions");
+};
+exports.getPharmacyDrugs = (req, res) => {
+  res.render("categories/pharmacy-drugs");
+};
+exports.getPharmacyContracts = (req, res) => {
+  res.render("categories/pharmacy-contracts");
+};
+
 
--- routes/tableRouter.js
+++ routes/tableRouter.js
@@ -4,13 +4,13 @@ const tableRouter = express.Router();
 const tableController = require("../controllers/tableController");
 
 tableRouter.get("/", tableController.getIndex);
-// tableRouter.get("/doctors", tableController.getDoctors);
-// tableRouter.get("/patients", tableController.getPatients);
-// tableRouter.get("/pharmaceutical-companies", tableController.getPharmaceuticalCompanies);
-// tableRouter.get("/drugs", tableController.getDrugs);
-// tableRouter.get("/pharmacies", tableController.getPharmacies);
-// tableRouter.get("/prescriptions", tableController.getPrescriptions);
-// tableRouter.get("/pharmacy-drugs", tableController.getPharmacyDrugs);
-// tableRouter.get("/pharmacy-contracts", tableController.getPharmacyContracts);
+tableRouter.get("/doctors", tableController.getDoctors);
+tableRouter.get("/patients", tableController.getPatients);
+tableRouter.get("/pharmaceutical-companies", tableController.getPharmaceuticalCompanies);
+tableRouter.get("/drugs", tableController.getDrugs);
+tableRouter.get("/pharmacies", tableController.getPharmacies);
+tableRouter.get("/prescriptions", tableController.getPrescriptions);
+tableRouter.get("/pharmacy-drugs", tableController.getPharmacyDrugs);
+tableRouter.get("/pharmacy-contracts", tableController.getPharmacyContracts);
 
 module.exports = tableRouter;
